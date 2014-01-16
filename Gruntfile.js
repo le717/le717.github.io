925c4420e4448e0dc029aa99ce5a9277c5e32c9c
Triangle717 <le717@hotmail.com>
2014-01-16 17:12:33 -0500

    Add cssmin dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,46 @@
 module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
+    // Project configuration.
+    grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
     jshint: {
-      // define the files to lint
-      files: ["gruntfile.js", "js/*.js"],
-      // configure JSHint (documented at http://www.jshint.com/docs/)
-      options: {
-          // more options here if you want to override JSHint defaults
-        globals: {
-          jQuery: true
+        // Define the JS files to lint
+        files: ["gruntfile.js", "js/script.js"],
+        options: {
+            globals: {
+                jQuery: true
+            }
         }
-      }
+    },
+    cssmin: {
+        add_banner: {
+            options: {
+                banner: "/* Created 2014 Triangle717, Licensed under The MIT License */"
+            },
+            files: {
+                "css/style.min.css": ["css/style.css"]
+            }
+        }
     },
     uglify: {
-      my_target: {
-        files: {
-          //'js/<%= pkg.name %>.min.js': ['js/<%= pkg.name %>.js']
-          'js/script.min.js': ['js/script.js'],
-          'css/style.min.css': ['css/style.css']
+        my_target: {
+            files: {
+                "js/script.min.js": ["js/script.js"],
+            }
         }
-      }
     },
     watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint', 'uglify']
-    }
-  });
-    
-//'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        files: ["<%= jshint.files %>"],
+            tasks: ["jshint", "uglify"]
+        }
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks("grunt-contrib-watch");
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-watch");
 
-  // Default task(s).
-  grunt.registerTask("default", ["jshint", "uglify"]);
+    // Default task(s).
+    grunt.registerTask("default", ["watch", "cssmin", "jshint", "uglify"]);
 
 };
