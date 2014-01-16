903f1dd6604fb472928f9f12bf4ca283bf42999a
Triangle717 <le717@hotmail.com>
2014-01-16 15:20:14 -0500

    Split JavaScript into separate file

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/*
+    Created 2014 Triangle717
+    <http://Triangle717.WordPress.com/>
+
+    Licensed under The MIT License
+    <http://opensource.org/licenses/MIT/>
+*/
+
+function stopAll() {
+    "use strict";
+    /* Stop the animation and display (pittyful) page contents */
+    var aniOne = document.querySelector(".slide-ani");
+    var aniTwo = document.querySelector(".rotate-ani");
+    var racketAni = document.querySelector(".racket-ani");
+    var waitLink = document.querySelector("#wait-link");
+    var loadingText = document.querySelector("#loading p");
+    var pageContent = document.querySelector("#subpages");
+    var wrapper = document.querySelector("#wrapper");
+
+    // Stop animations
+    aniOne.style.animationPlayState = "paused";
+    aniTwo.style.animationPlayState = "paused";
+    racketAni.style.animationPlayState = "paused";
+
+    // Stop animations on Webkit too (Grrr Chrome)
+    aniOne.style.webkitAnimationPlayState = "paused";
+    aniTwo.style.webkitAnimationPlayState = "paused";
+    racketAni.style.webkitAnimationPlayState = "paused";
+
+    // Hide animation elements
+    waitLink.style.display = "none";
+    wrapper.style.display = "none";
+
+    // Display (lame) page contents
+    pageContent.style.display = "table";
+
+    //FIXME Page positioning so this is not needed on mobile (usually has no effect on desktop)
+    window.scroll(365, 0);
+}
