774a26641eecf1ce69c7b0e35383c7c39812e5ce
Triangle717 <le717@users.sourceforge.net>
2014-03-28 15:11:32 -0400

    Indent JS using two space soft tabs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,119 +1,122 @@
 module.exports = function(grunt) {
-    "use strict";
-    // Project configuration
-    grunt.initConfig({
-        pkg: grunt.file.readJSON("package.json"),
-        banner: '/* <%= pkg.name %> - v<%= pkg.version %>\n' +
-      '<%= pkg.homepage ? "" + pkg.homepage + "\\n" : "" %>' +
-      'Created <%= grunt.template.today("yyyy") %> <%= pkg.author %>; ' +
-      'Licensed under the <%= _.pluck(pkg.licenses, "type").join(", ") %>\n*/\n',
-        cssfiles: ["css/*.css", "!css/*.min.css"],
-        jsfiles: ["js/*.js", "!js/*.min.js"],
+  "use strict";
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    banner: '/* <%= pkg.name %> - v<%= pkg.version %>\n' +
+    '<%= pkg.homepage ? "" + pkg.homepage + "\\n" : "" %>' +
+    'Created <%= grunt.template.today("yyyy") %> <%= pkg.author %>; ' +
+    'Licensed under the <%= _.pluck(pkg.licenses, "type").join(", ") %>\n*/\n',
+    cssfiles: ["css/*.css", "!css/*.min.css"],
+    jsfiles: ["js/*.js", "!js/*.min.js"],
 
-        // Keep the devDependencies up-to-date
-        devUpdate: {
-            main: {
-                options: {
-                    // Do not mention already updated dependencies
-                    reportUpdated: false,
-                    // Prompt asking if the dependency should be updated
-                    updateType : "prompt"
-                }
-            }
-        },
-
-        // Copy JavaScript dependencies to the proper location
-        copy: {
-            main: {
-                expand: true,
-                cwd: "node_modules/",
-                src: ["jquery.browser/dist/*.min.js", "string-format/lib/*"],
-                dest: "lib/",
-                flatten: true,
-                filter: "isFile"
-            },
-        },
-
-        // Lint the HTML using HTMLHint
-        htmlhint: {
-            html: {
-                options: {
-                    "tag-pair": true,
-                },
-                src: ["index.html"]
-            }
-        },
-
-        // Lint the CSS using CSS Lint
-        csslint: {
-            strict: {
-                options: {
-                    csslintrc: ".csslintrc",
-                    "import": 2
-                },
-                src: "<%= cssfiles %>",
-            }
-        },
-
-        // Minify any CSS using CSSMin
-        cssmin: {
-            add_banner: {
-                options: {
-                    banner: "<%= banner %>"
-                },
-                files: {
-                    "css/<%= pkg.name %>.min.css": "<%= cssfiles %>"
-                }
-            }
-        },
-
-        // Lint the JavaScript using JSHint
-        jshint: {
-            src: {
-                options: {
-                    jshintrc: ".jshintrc"
-                },
-                src: "<%= jsfiles %>",
-            },
-        },
-
-        // Minify any JavaScript using Uglify
-        uglify: {
-            options: {
-                banner: "<%= banner %>",
-                compress: true,
-                report: "min"
-            },
-            my_target: {
-                files: {
-                    "js/<%= pkg.name %>.min.js": "<%= jsfiles %>",
-                }
-            }
-        },
-
-        // Watched files to trigger grunt
-        watch: {
-            files: ["Gruntfile.js", "<%= cssfiles %>", "<%= jsfiles %>"],
-            tasks: ["all"]
+    // Keep the devDependencies up-to-date
+    devUpdate: {
+      main: {
+        options: {
+          // Do not mention already updated dependencies
+          reportUpdated: false,
+          // Prompt asking if the dependency should be updated
+          updateType : "prompt"
         }
-    });
+      }
+    },
 
-    // Load all the plugins required to perform our tasks
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    // Copy JavaScript dependencies to the proper location
+    copy: {
+      main: {
+        expand: true,
+        cwd: "node_modules/",
+        src: ["jquery.browser/dist/*.min.js", "string-format/lib/*"],
+        dest: "lib/",
+        flatten: true,
+        filter: "isFile"
+      },
+    },
 
-    grunt.registerTask('default', 'List commands', function () {
-        grunt.log.writeln("");
-        grunt.log.writeln('Run "grunt lint" to lint the source files');
-        grunt.log.writeln('Run "grunt build" to minify the source files');
-        grunt.log.writeln('Run "grunt devUpdate" to update the devDependencies');
-        grunt.log.writeln('Run "grunt all" to run all tasks except "devUpdate"');
-    });
+    // Lint the HTML using HTMLHint
+    htmlhint: {
+      html: {
+        options: {
+          "tag-pair": true,
+        },
+        src: ["index.html"]
+      }
+    },
 
-    // Define the tasks
-    grunt.registerTask("lint", ["htmlhint", "csslint", "jshint"]);
-    grunt.registerTask("build", ["cssmin", "uglify", "copy"]);
-    grunt.registerTask("all", ["lint", "build"]);
+    // Lint the CSS using CSS Lint
+    csslint: {
+      strict: {
+        options: {
+          csslintrc: ".csslintrc",
+          "import": 2
+        },
+        src: "<%= cssfiles %>",
+      }
+    },
 
-    // Always use --force to stop csslint from killing the task
-    grunt.option("force", true);
+    // Minify any CSS using CSSMin
+    cssmin: {
+      add_banner: {
+        options: {
+          banner: "<%= banner %>"
+        },
+        files: {
+          //"css/<%= pkg.name %>.min.css": "<%= cssfiles %>"
+          // FIXME THIS IS NOT IDEAL
+          "css/not-pong.min.css": "css/not-pong.css",
+          "css/<%= pkg.name %>.min.css": "css/style.css"
+        }
+      }
+    },
+
+    // Lint the JavaScript using JSHint
+    jshint: {
+      src: {
+        options: {
+          jshintrc: ".jshintrc"
+        },
+        src: "<%= jsfiles %>",
+      },
+    },
+
+    // Minify any JavaScript using Uglify
+    uglify: {
+      options: {
+        banner: "<%= banner %>",
+        compress: true,
+        report: "min"
+      },
+      my_target: {
+        files: {
+          "js/<%= pkg.name %>.min.js": "<%= jsfiles %>",
+        }
+      }
+    },
+
+    // Watched files to trigger grunt
+    watch: {
+      files: ["Gruntfile.js", "<%= cssfiles %>", "<%= jsfiles %>"],
+      tasks: ["all"]
+    }
+  });
+
+  // Load all the plugins required to perform our tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.registerTask('default', 'List commands', function () {
+    grunt.log.writeln("");
+    grunt.log.writeln('Run "grunt lint" to lint the source files');
+    grunt.log.writeln('Run "grunt build" to minify the source files');
+    grunt.log.writeln('Run "grunt devUpdate" to update the devDependencies');
+    grunt.log.writeln('Run "grunt all" to run all tasks except "devUpdate"');
+  });
+
+  // Define the tasks
+  grunt.registerTask("lint", ["htmlhint", "csslint", "jshint"]);
+  grunt.registerTask("build", ["cssmin", "uglify", "copy"]);
+  grunt.registerTask("all", ["lint", "build"]);
+
+  // Always use --force to stop csslint from killing the task
+  grunt.option("force", true);
 };
--- js/script.js
+++ js/script.js
@@ -1,83 +1,82 @@
 /*
-    Created 2014 Triangle717
-    <http://Triangle717.WordPress.com/>
+  Created 2014 Triangle717
+  <http://Triangle717.WordPress.com/>
 
-    Licensed under The MIT License
-    <http://opensource.org/licenses/MIT/>
+  Licensed under The MIT License
+  <http://opensource.org/licenses/MIT/>
 */
 
 var $userBrowser = $("#yourBrowser");
 
 
 function hideBrowserBox() {
-    "use strict";
-    /* Hide the browser box */
-    // Trigger the fade-out transition
-    $userBrowser.css("opacity", "0");
+  "use strict";
+  /* Hide the browser box */
+  // Trigger the fade-out transition
+  $userBrowser.css("opacity", "0");
 }
 
 
 function detectBrowser() {
-    "use strict";
-    /* Detect the user's web browser and display message */
-    var theBrowser,
-        theBrowserLogo,
-        $theBrowserVersion = $.browser.versionNumber,
+  "use strict";
+  /* Detect the user's web browser and display message */
+  var theBrowser, theBrowserLogo,
+      $theBrowserVersion = $.browser.versionNumber,
 
-        // Default compatibility message
-        theBrowserMessage = "You should be able to view my site error free!<br>",
+      // Default compatibility message
+      theBrowserMessage = "You should be able to view my site error free!<br>",
 
-        // Message to display for incompatible browser
-        theBrowserMessageError = "You might experience some issues while browsing my site. " +
-        "If you do, please report them for me to fix.";
+      // Message to display for incompatible browser
+      theBrowserMessageError = "You might experience some issues while browsing my site. " +
+      "If you do, please report them for me to fix.";
 
-    // This is Google Chrome
-    if ($.browser.chrome) {
-        theBrowser = "Google Chrome";
-        theBrowserLogo = "img/chrome.png";
+  // This is Google Chrome
+  if ($.browser.chrome) {
+    theBrowser = "Google Chrome";
+    theBrowserLogo = "img/chrome.png";
 
-    } else if ($.browser.mozilla) {
-        // This is Mozilla Firefox
-        theBrowser = "Mozilla Firefox";
-        theBrowserLogo = "img/firefox.png";
+  } else if ($.browser.mozilla) {
+    // This is Mozilla Firefox
+    theBrowser = "Mozilla Firefox";
+    theBrowserLogo = "img/firefox.png";
 
-    } else if ($.browser.safari) {
-        // This is Safari
-        theBrowser = "Apple Safari";
-        theBrowserLogo = "img/safari.png";
-        theBrowserMessage = theBrowserMessageError;
+  } else if ($.browser.safari) {
+    // This is Safari
+    theBrowser = "Apple Safari";
+    theBrowserLogo = "img/safari.png";
+    theBrowserMessage = theBrowserMessageError;
 
-        // Display error message for Safari 5 and below
-        if ($theBrowserVersion <= 5) {
-            theBrowserMessage = "Your version of Safari does not support my site. " +
-            "Please visit browsehappy.com to research a modern browser.";
-        }
-
-    } else if ($.browser.opr) {
-        // This is Opera
-        theBrowser = "Opera";
-        theBrowserLogo = "img/opera.png";
-
-    } else if ($.browser.msie) {
-        // This is Internet Explorer
-        theBrowser = "Internet Explorer";
-        theBrowserLogo = "img/ie.png";
-
-        //TODO IE11 on Windows 7 is broken, but IE11 on Win8.1 works? Huh?
-        // Display error message for IE 9 and below
-        if ($theBrowserVersion <= 9) {
-            theBrowserMessage = "Your version of IE does not support my site. " +
-            "Please visit browsehappy.com to research a modern browser.";
-        }
-
-    } else {
-        // Some other browser
-        theBrowser = "An Unidentified Browser";
-        $theBrowserVersion = "";
-        theBrowserLogo = "img/globe-blue.png";
-        theBrowserMessage = "If you will, please submit an issue on GitHub with compatibility " +
-            "results so I can develop for this browser.";
+    // Display error message for Safari 5 and below
+    if ($theBrowserVersion <= 5) {
+      theBrowserMessage = "Your version of Safari does not support my site. " +
+      "Please visit browsehappy.com to research a modern browser.";
     }
+
+  } else if ($.browser.opr) {
+    // This is Opera
+    theBrowser = "Opera";
+    theBrowserLogo = "img/opera.png";
+
+  } else if ($.browser.msie) {
+    // This is Internet Explorer
+    theBrowser = "Internet Explorer";
+    theBrowserLogo = "img/ie.png";
+
+    //TODO IE11 on Windows 7 is broken, but IE11 on Win8.1 works? Huh?
+    // Display error message for IE 9 and below
+    if ($theBrowserVersion <= 9) {
+      theBrowserMessage = "Your version of IE does not support my site. " +
+      "Please visit browsehappy.com to research a modern browser.";
+    }
+
+  } else {
+    // Some other browser
+    theBrowser = "An Unidentified Browser";
+    $theBrowserVersion = "";
+    theBrowserLogo = "img/globe-blue.png";
+    theBrowserMessage = "If you will, please submit an issue on GitHub with compatibility " +
+        "results so I can develop for this browser.";
+  }
 
     // Insert message and browser logo
     /* jshint ignore:start */
@@ -85,19 +84,19 @@ function detectBrowser() {
    theBrowser, $theBrowserVersion, theBrowserMessage, theBrowserLogo));
     /* jshint ignore:end */
 
-    // Trigger the fade-in transitions
-    $userBrowser.css("transform", "translateY(-230px)");
+  // Trigger the fade-in transitions
+  $userBrowser.css("transform", "translateY(-230px)");
 }
 
 $(function() {
-    // Run function to detect visitor's browser
-    detectBrowser();
+  // Run function to detect visitor's browser
+  detectBrowser();
 
-    // Get date of last commit using GitHub Pages API
-    $.getJSON("https://api.github.com/repos/le717/le717.github.io",
-              function(data) {
-                    var lastUpdate = data.updated_at.replace(/(T(.*))$/g, "");
-                    //var lastUpdate = "Right now";
-                    $("#last-update").text("Site last updated on: {0}".format(lastUpdate));
-                }, "json");
+  // Get date of last commit using GitHub Pages API
+  $.getJSON("https://api.github.com/repos/le717/le717.github.io",
+    function(data) {
+      var lastUpdate = data.updated_at.replace(/(T(.*))$/g, "");
+      //var lastUpdate = "Right now";
+      $("#last-update").text("Site last updated on: {0}".format(lastUpdate));
+    }, "json");
 });
