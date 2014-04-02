6dbdcd652567b632353a8e3164edd48c1225ccb1
Triangle717 <le717@users.sourceforge.net>
2014-04-02 14:21:56 -0400

    Up next: The text *shudder*

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ function detectBrowser() {
       showPanel = true;
     }
 
-  } else if (!$.browser.chrome /*&& !$.browser.mozilla*/ && !$.browser.opr && !$.browser.cros) {
+  } else if (!$.browser.chrome && !$.browser.mozilla && !$.browser.opr && !$.browser.cros) {
     // Some other browser
     theBrowser = "an unidentified browser";
     $theBrowserVersion = "";
