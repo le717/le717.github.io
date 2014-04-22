le717/le717.github.io

commit d473fd28c2c584a7f207b8f1410519d25fbc394c

Limit the amount of blog posts downloaded on mobile [ci skip]

blog/index.html
@@ -6,6 +6,7 @@ redirect_from: /blog.html
 
 {% include loading.html %}
 <div class="blog-posts"></div>
+<p id="end-of-posts"></p>
 
 <script>
   console.log("Loading blog feed from http://Triangle717.WordPress.com");
@@ -31,7 +32,7 @@ redirect_from: /blog.html
   function BlogPost(postAuthor, postDate, postID, postTitle, postURL, postContent) {
     this.postAuthor       = postAuthor.name;
     this.postAuthorAvatar = postAuthor.avatar_URL.replace(/s=96/, "s=40");
-    this.postContainer    = '<div id="{0}"></div>'.format(postID);
+    this.postContainer    = '<div id="' + postID + '"></div>';
     this.postContent      = postContent;
     this.postDate         = postDate;
     this.postID           = postID;
@@ -45,8 +46,17 @@ redirect_from: /blog.html
     /* Create a blog post object
      * Based on example from http://jsfiddle.net/hLWdH/334/
      */
-    $.getJSON("//public-api.wordpress.com/rest/v1/sites/triangle717.wordpress.com/posts/?number=7&callback=?", function(data) {
-      console.log("{0} blog posts retrieved".format(data.posts.length));
+
+    // Download a different number of posts on different platforms.
+    // Downloading seven posts on mobile might be enough to crash the browser on mobile.
+    if ($.browser.deskop) {
+      var postsToDownload = 7;
+    } else {
+      var postsToDownload = 4;
+    }
+
+    $.getJSON("//public-api.wordpress.com/rest/v1/sites/triangle717.wordpress.com/posts/?number={0}&callback=?".format(postsToDownload), function(data) {
+      console.log("{0} blog posts retrieved".format(postsToDownload));
 
       data.posts.forEach(function(postInfo) {
         // Create a unique post ID
@@ -61,12 +71,15 @@ redirect_from: /blog.html
         // Create the post object and store it
         var myPost = new BlogPost(postInfo.author, postDate, postID, postInfo.title, postInfo.guid, postInfo.content);
         allBlogPosts.push(myPost);
+        allBlogPosts.push(myPost);
       });
 
       // Now that the posts are stored, go through and display them and stop the loading animation
       showPosts();
       $(".loading-ani").remove();
       $(".blog-posts").css("display", "");
+
+      // Shrink the images down a bit
       shrinkImages();
       });
     })();
@@ -75,6 +88,8 @@ redirect_from: /blog.html
       "use strict";
       /* Display each blog post */
       $.each(allBlogPosts, function(key, value) {
+
+      value.cleanUp();
 
       // Add the post container to the page
       $(".blog-posts").append(value.postContainer);
@@ -110,10 +125,10 @@ redirect_from: /blog.html
     });
   }
 
-  /*BlogPost.prototype.cleanUp = function() {
-   * "use strict";
-
-  };*/
+  BlogPost.prototype.cleanUp = function() {
+   "use strict";
+    console.log(this);
+  };
 
 
   function cleanupPost(postContainer) {

js/script.js
@@ -9,7 +9,6 @@
 function showSidebar() {
   "use strict";
   /* Display the site sidebar */
-
   $("#sidebar").toggleClass("sidebar-hidden");
 }
 
@@ -71,7 +70,7 @@ function detectBrowser() {
     // IE 9 does not like the text-shadow
     $(".announcement a").css("color", "black");
 
-  } else if (!$.browser.chrome && !$.browser.mozilla && !$.browser.safari &&
+  } else if (!$.browser.chrome /*&& !$.browser.mozilla*/ && !$.browser.safari &&
              !$.browser.msie && !$.browser.opr && !$.browser.cros) {
     // Some other browser
     theBrowser = "an unidentified browser";
@@ -106,6 +105,8 @@ function shrinkImages() {
     if ($.browser.mobile) {
       if (!$this.hasClass("no-mobile-resize") && !$this.hasClass("emoji")) {
         // However, do it only if the image is wider than the screen
+        console.log($this.width());
+        console.log($(window).width());
         if ($this.width() >= $(window).width()) {
           $this.width($this.width() / 3);
           $this.height($this.height() / 3);

_includes/loading.html
@@ -1,2 +1,2 @@
 <!-- Loading image -->
-  <img class="fully-centered loading-ani no-mobile-resize ie-svg" width="500" height="500" src="/img/Triangle717-Logo.svg" />
+  <img class="fully-centered loading-ani no-mobile-resize ie-svg" alt="Loading image" width="500" height="500" src="/img/Triangle717-Logo.svg" />

css/animations.css
@@ -59,6 +59,7 @@
 
   100% {
     display: none;
+    opacity: 0;
   }
 }
 
@@ -74,5 +75,6 @@
 
   100% {
     display: none;
+    opacity: 0;
   }
 }

commit fab183f290e449d119f686439c0b9355c30559f0

Found it. Bug introduced in d473fd28c2c584a7f207b8f1410519d25fbc394c
ugh.......

_layouts/default.html
@@ -5,4 +5,5 @@
   {% include sidebar.html %}
   {{ content }}
 {% if page.type != "Blog" %}{% include scripts.html %}{% endif %}
+{% include scrollup.html %}
 {% include foot.html %}

_layouts/story.html
@@ -7,4 +7,5 @@
 <p class="publish-info">Published on {{ page.publish-date }} by {{ page.author }}</p>
   {{ content }}
 {% include scripts.html %}
+{% include scrollup.html %}
 {% include foot.html %}

blog/index.html
@@ -58,10 +58,8 @@ redirect_from: /blog.html
     } else {
       postsToDownload = 4;
     }
-    console.log("{0} blog posts to be retrieved".format(postsToDownload));
 
-//    $.getJSON("//public-api.wordpress.com/rest/v1/sites/triangle717.wordpress.com/posts/?number=" + postsToDownload + "&callback=?", function(data) {
-    $.getJSON("//public-api.wordpress.com/rest/v1/sites/triangle717.wordpress.com/posts/?number=7&callback=?", function(data) {
+    $.getJSON("//public-api.wordpress.com/rest/v1/sites/triangle717.wordpress.com/posts/?number=" + postsToDownload + "&callback=?", function(data) {
       console.log("{0} blog posts retrieved".format(postsToDownload));
 
       data.posts.forEach(function(postInfo) {
@@ -76,7 +74,6 @@ redirect_from: /blog.html
 
         // Create the post object and store it
         var myPost = new BlogPost(postInfo.author, postDate, postID, postInfo.title, postInfo.guid, postInfo.content);
-        allBlogPosts.push(myPost);
         allBlogPosts.push(myPost);
       });
 

js/script.js
@@ -20,6 +20,10 @@ function showSidebarBtn() {
     if ($(window).width() >= 320 && $(window).width() <= 480 ) {
       $("#sidebar-btn").addClass("sidebar-btn-visible");
       $("#sidebar").addClass("sidebar-hidden");
+
+    } else {
+      $("#sidebar-btn").removeClass("sidebar-btn-visible");
+      $("#sidebar").removeClass("sidebar-hidden");
     }
   });
 }
@@ -120,7 +124,7 @@ $(function() {
 
   shrinkImages();
   // Run process to detect visitor's browser
-  detectBrowser();
+  //detectBrowser();
 
   // Replace the SVG with a PNG on IE (IE HATES SVGs)
   if ($.browser.msie) {