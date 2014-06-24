a7d462a795ed40f884adb0a2e619eba281e1db98
Triangle717 <le717.code@yahoo.com>
2014-06-24 11:36:32 -0400

    Update live with updated spoiler code

--- js/script.js
+++ js/script.js
@@ -85,50 +85,58 @@
 
     /* ------- Spoiler handling ------- */
 
-    var spoilerHeights = [],
-        spoilerID      = 0,
-        spoilerBtnID   = 0,
-        $spoilerClass  = $(".spoiler");
+    // Variables for usage
+    var spoilerID          = 0,
+        btnSpoilerID       = 0,
+        spoilerHeights     = [],
+        paddingValue       = "5px",
+        btnSpoiler         = "btn-spoiler",
+        spoiler            = "spoiler",
+        spoilerShownClass  = "spoiler-shown",
+        $btnSpoiler        = $(".btn-spoiler"),
+        $spoiler           = $(".spoiler"),
+        spoilerActiveClass = "btn-spoiler-active";
 
     // Add IDs to each spoiler
-    $spoilerClass.each(function(index, value) {
-      $(value).attr("id", "spoiler-" + spoilerID);
+    $spoiler.each(function(index, value) {
+      $(value).attr("id", spoiler + "-" + spoilerID);
       spoilerID += 1;
 
       // Get the height of the content to be spoilered now,
       // as once we hide the text it cannot be restored.
-      // Add 5px for a bit of extra space to prevent text getting cutting off
-      spoilerHeights.push(parseInt($(value).css("height").slice(0, -2)) + 5 + "px");
+      // Add `paddingValue` for a bit of extra space to
+      // prevent content from being cut off.
+      spoilerHeights.push(parseInt($(value).css("height").slice(0, -2)) + paddingValue);
     });
 
-    // Add the toggle button (we need it for this to even work :P)
-    $spoilerClass.before("<div class='spoiler-btn'><span>Spoiler</span></div>");
+    // Add the toggle button
+    $spoiler.before("<div class='" + btnSpoiler + "'><span>Spoiler</span></div>");
 
     // Add matching IDs to each toggle button
-    $(".spoiler-btn").each(function(index, value) {
-      $(value).attr("id", "spoiler-btn-" + spoilerBtnID);
-      spoilerBtnID += 1;
+    $btnSpoiler.each(function(index, value) {
+      $(value).attr("id", btnSpoiler + "-" + btnSpoilerID);
+      btnSpoilerID += 1;
     });
 
-    // Now that we have the height, hide the text
-    $spoilerClass.css("height", "0");
+    // Now that we have the height, hide all content
+    $spoiler.css("height", "0");
 
-    $(".spoiler-btn").on("click", function() {
+    $btnSpoiler.on("click", function() {
       // Get the ID for the clicked spoiler button so only that one is triggered
-      var thisSpoilerID = "#" + $(this).attr("id").replace(/-btn/, ""),
+      var thisSpoilerID = "#" + $(this).attr("id").replace(/btn-/, ""),
           thisSpoilerNumber = thisSpoilerID.slice(thisSpoilerID.length - 1),
           $thisSpoiler  = $(thisSpoilerID);
 
       // Upon clicking the spoiler button,
       // if the content is hidden, reveal it.
       // Otherwise, hide it.
-      if ($thisSpoiler.hasClass("spoiler-shown")) {
+      if ($thisSpoiler.hasClass(spoilerShownClass)) {
         $thisSpoiler.css("height", "0");
       } else {
         $thisSpoiler.css("height", spoilerHeights[thisSpoilerNumber]);
       }
-      $thisSpoiler.toggleClass("spoiler-shown");
-      $(this).toggleClass("spoiler-btn-active");
+      $thisSpoiler.toggleClass(spoilerShownClass);
+      $(this).toggleClass(spoilerActiveClass);
     });
   });
 })(jQuery);
