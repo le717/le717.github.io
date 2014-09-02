90d37a823ded966ea3d0b68146d8e72776e66b68
Triangle717 <le717.code@yahoo.com>
2014-09-02 09:42:15 -0400

    More minified things

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,7 +86,7 @@ module.exports = function(grunt) {
         banner: "<%= banner %>",
         compress: true,
         report: "min",
-        sourceMap: true
+        sourceMap: false
       },
       my_target: {
         files: {
