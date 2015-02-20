cdfd5fd92143e3fc4b00cb1cabc23a165fc787be
Triangle717 <le717.code@yahoo.com>
2015-02-19 23:22:48 -0500

    Display warning of Lenovo Superfish on all pages

--- js/lenovo.js
+++ js/lenovo.js
@@ -6,19 +6,16 @@
   // From https://paste.ee/p/y1RvZ and https://justgetflux.com/
   // Modified by le717
 
-  // TODO Testing
-  window.similarproducts = {};
-
   function checkLenovo() {
-    // this variable is defined as soon as the malicious payload from Superfish is injected into the DOM
+    // This variable is defined as soon as the malicious payload from Superfish is injected into the DOM
     if (window.similarproducts) {
-      // display a warning to your visitor that they've been hacked by Lenovo
+      // Display a warning to your visitor that they've been hacked by Lenovo
       $(".page-wrapper").prepend('{% include lenovo-min.html %}');
 
       // Add button listeners
       setTimeout(function() {
         $(".lenovo .btn.remove").on("click", function() {
-          window.location.href = "https://blog.lastpass.com/2015/02/are-you-at-risk-from-superfish-check-now.html/";
+          window.location.href = "https://lastpass.com/superfish/";
         });
 
         $(".lenovo .btn.close").on("click", function() {
