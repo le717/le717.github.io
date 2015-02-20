e0b2e9a291f5e1a81d9338e318f5b3370dd31e19
Triangle717 <le717.code@yahoo.com>
2015-02-19 22:44:29 -0500

    . [ci skip]

--- js/lenovo.js
+++ js/lenovo.js
@@ -1,29 +1,21 @@
 (function($) {
   "use strict";
+  // From https://paste.ee/p/y1RvZ and https://justgetflux.com/
+  // Modified by le717
+
   // TODO Testing
   window.similarproducts = {};
 
-  // From https://paste.ee/p/y1RvZ and https://justgetflux.com/
-  var ccounter = 0,
-      check_lenovo_iv = setInterval(checkLenovo, 2000);
-
   function checkLenovo() {
-    ccounter++;
     if (window.similarproducts) { // this variable is defined as soon as the malicious payload from Superfish is injected into the DOM
       // display a warning to your visitor that they've been hacked by Lenovo
-
       $(".page-wrapper").append("{% include lenovo.html %}");
-
-      // ok let's do this:
-//      setTimeout(function() {
-//        window.location.href = "http://news.lenovo.com/images/20034/remove-superfish-instructions.pdf";
-//      }, 1500);
     }
-
-    if (ccounter > 20) clearInterval(check_lenovo_iv);
   }
 
   $(".lenovo .btn.close").on("click", function() {
     $(".lenovo").remove();
   });
+
+  window.setTimeout(checkLenovo, 5000);
 })(jQuery);
