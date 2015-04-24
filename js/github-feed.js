b3a51071fba26fb0f4902aebeeca92cf73455520
Triangle717 <le717.code@yahoo.com>
2015-04-24 17:29:54 -0400

    Update _createDate()

--- js/github-feed.js
+++ js/github-feed.js
@@ -114,14 +114,14 @@
    * @param {String} rawDate GitHub timestamp.
    * @returns {String}.
    */
-  function _createDate(rawDate) {
+  function _createDate(date) {
     var months = {"01": "January", "02": "February", "03": "March", "04": "April",
                   "05": "May", "06": "June", "07": "July", "08": "August", "09": "September",
                   "10": "October", "11": "November", "12": "December"
                  };
 
-    var dateParts  = rawDate.split("T")[0].split("-"),
-        dateFormat = "{0} {1} {2}".format(dateParts[2], dateParts[1], dateParts[0]).split(" ");
+    var dateParts  = date.split("T")[0].split("-"),
+        dateFormat = [dateParts[2], dateParts[1], dateParts[0]];
     dateFormat[1]  = months[dateFormat[1]] + ",";
     return dateFormat.join(" ");
   }
