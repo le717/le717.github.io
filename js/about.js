2e64b6987e9f71a131de6f5aa02c76b730e043b4
Triangle717 <le717.code@yahoo.com>
2015-08-01 21:07:18 -0400

    Update about.js

--- js/about.js
+++ js/about.js
@@ -12,26 +12,27 @@
  * @param {Object.<number>} date The starting date to calculate from.
  *                               The object contains three numeric keys, year, month, and day.
  *                               The year is expressed in four digits, e.g., 2015.
- * @returns {Number}.
+ * @returns {Number}
  */
-function calcDate(date) {
+function yearDifference(date) {
   "use strict";
-  var curDate  = new Date();
-  var curDay   = curDate.getUTCDate(),
-      curYear  = curDate.getUTCFullYear(),
-      curMonth = curDate.getUTCMonth() + 1,  // UTC month value is zero-based
-      myCurAge = curYear % date.year;
+  var curDate = new Date(),
+      now     = {
+        year: curDate.getUTCFullYear(),
+        // UTC month value is zero-based
+        month: curDate.getUTCMonth() + 1,
+        day: curDate.getUTCDate()
+      },
+      diff = now.year % date.year;
 
   // Do not update the date unless it is time
-  if (curMonth < date.month && curDay > date.day ||
-      curMonth < date.month && curDay < date.day ||
-      curMonth < date.month && curDay === date.day ||
-      curMonth === date.month && curDay < date.day) {
-    myCurAge -= 1;
+  if (now.month < date.month ||
+      now.month === date.month && now.day < date.day) {
+    diff -= 1;
   }
 
-  return myCurAge;
+  return diff;
 }
 
 // Calculate my age
-document.querySelector("#my-age").innerHTML = calcDate({ year: 1995, month: 3, day: 13 });
+document.querySelector("#my-age").innerHTML = yearDifference({ year: 1995, month: 3, day: 13 });
